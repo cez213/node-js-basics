a1e0fc00a0091f60d90f0be80627f20cd19f095c
Carolyn Zelenetz <carolyn.zelenetz@gmail.com>
2015-06-01 17:16:58 -0400

    Revised app.js to require profile.js

--- app.js
+++ app.js
@@ -1,1 +1,5 @@
-console.log("Hello, World");
+//Note: .js at the end of profile is optional, but path is mandatory
+var profile = require("./profile.js");
+var users = process.argv.slice(2);
+//var users = ["carolynzelenetz", "chalkers"];
+users.forEach(profile.get);
